037dbbd46e0381fc642f0c4c0760d2ac24c4b2a4
KuanYu Chu <ckybonist@gmail.com>
2016-06-19 13:08:21 +0800

    Update travel time calculation method
    Separate original calculation function into three parts:
        1. Each start time (every 5 minutes)
        2. Each hour
        3. Each day

--- sensor_section/travel_time.py
+++ sensor_section/travel_time.py
@@ -1,10 +1,5 @@
-g#!/usr/bin/env python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
-import os
-import sys
-import math
-from utils import CSVUtil
 
 """
     Input Data: TDCS_M04A_[date]_[time]
@@ -13,19 +8,29 @@ from utils import CSVUtil
         date_time,  etc_entrance, etc_exit, car_type, mean_travel_time, num_cars
 """
 
+import os
+import sys
+import math
+from collections import namedtuple, defaultdict
+
+from utils import CSVUtil
+
+
+## Constant
 DATA_EXT = '.csv'
 NUM_CAR_TYPE = 5
+INPUT = '../data/'
+OUTPUT = '../output/sensor_section/'
+SensorSection = namedtuple("SensorSection", ["entry", "exit"])  # where the magic happens
 
-def save(result, fname):
-    dirname = 'output'
-    file_ext = '.csv'
-    if dirname not in os.listdir('.'):
-        os.mkdir(dirname)
-    fname = os.path.splitext(fname)[0]  # remove file extension
-    fname = fname.split('_', 2)[-1]  # retain date from orignal file name
-    fname += '_s1tt' + file_ext
-    csv.write(result, dirname + '/' + fname)
 
+## Helper Functions
+def listdirNoHidden(path):
+    for f in os.listdir(path):
+        if not f.startswith('.'):
+            yield f
+
+### ====================== MAIN ============================
 """
     @ Calcuate mean travel time of all types of cars between two ETC stations by:
         * mean travel time (mtt_c)
@@ -40,42 +45,97 @@ def save(result, fname):
 
     @ Note:
         * if no car pass the ETC section, then the travel time will be:
-          ETC_section_distance(km) / 80(km/h) * 3600
-
+           1. -1
+           2. ETC_section_distance(km) / 80(km/h) * 3600
+           p.s.: #2 will apply when etc locations is settle down
 """
-def calc_s1tt(fname):
+def calcTravelTimeStartAt(fname):
+    csv = CSVUtil()
     data = csv.read(fname)
+    avg_travel_time = lambda nu, de: math.floor(nu / de)
+    result = []
+
     c = 0
     numerator = 0
     denominator = 0
-    result = []
+
     for row in data:
         c += 1
         numerator += int(row[4]) * int(row[5])
         denominator += int(row[5])
-        if c > 0 and c % NUM_CAR_TYPE == 0 and denominator > 0:
-            s1tt = math.floor(numerator / denominator)
-            result.append((row[1], row[2], s1tt))
+
+        if c > 0 and c % NUM_CAR_TYPE == 0:
+            travel_time = -1 if denominator == 0 \
+                             else avg_travel_time(numerator, denominator)
+
+            sensor_section = SensorSection(entry=row[1], exit=row[2])
+            result.append((sensor_section, travel_time))
             numerator = 0
             denominator = 0
-    save(result, fname)
+
+    return result
+
+def calcHourlyTravelTime(hour_dir):
+    files = listdirNoHidden(hour_dir)
+    prefix = './' + hour_dir + '/'
+    result = []
+    for f in files:
+        f = prefix + f
+        result.append(calcTravelTimeStartAt(f))
+    return result
+
+
+"""
+    return result which has format like:
+        { ETC_entry, ETC_exit, 0:00, 0:05, ..., 23:55 }
+        NOTE: "0:00" means average travel time at 0:00
+
+"""
+def calcDailyTravelTime(path):
+    os.chdir(path)
+    hour_dirs = listdirNoHidden('.')
+    tmpres = list(map(calcHourlyTravelTime, hour_dirs))
+    print(tmpres[0][0][:10])
+
+    result = defaultdict(list)  # where the magic happens
+    # for hd in hour_dirs:
+    #     files = listdirNoHidden(hd)
+    #     prefix = './' + hd + '/'
+    #     for f in files:
+    #         f = prefix + f
+    #         travel_time = calcTravelTimeStartAt(f)
+    #         for k, v in travel_time:
+    #             print("section: {}".format(k))
+    #             print("Avg Time: {}".format(v))
+    #             break
+    #         break
+    #     break
+    #return result
+
+def save(result, fname):
+    dirname = 'output'
+    file_ext = '.csv'
+    if dirname not in os.listdir('.'):
+        os.mkdir(dirname)
+    fname = os.path.splitext(fname)[0]  # remove file extension
+    fname = fname.split('_', 2)[-1]  # retain date from orignal file name
+    fname += '_s1tt' + file_ext
+    csv.write(result, dirname + '/' + fname)
+
 
 def run(arg):
-    csv = CSVUtil()
-    if arg.endswith(DATA_EXT):  # file
-        fname = arg
-        calc_s1tt(fname)
-    else:  # dir
-        os.chdir(arg)
-        files = os.listdir('.')
-        for fname in files:
-            if fname.endswith(DATA_EXT):
-                calc_s1tt(fname)
+    if arg == "batch":
+        pass
+    elif arg == "test":  # for testing
+        date = "20150702"
+        path = INPUT + "201507/" + date
+        result = calcDailyTravelTime(path)
+
+### ============= End Main =======================
 
 
 
 if __name__ == "__main__":
-    csv = CSVUtil()
     args = sys.argv[1:]
 
     if not args:
