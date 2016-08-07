cf4bf135f08695dd2bc2f9b7dba656812a6e00ab
KuanYu Chu <ckybonist@gmail.com>
2016-08-07 13:15:57 +0800

    update main module

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-#from sensors import step1
+import time
+
+from sensor import Sensor as Step1
 from interchange import Interchange as Step2
-from path import PATHS
-from config import *
+from path import Path as Step3
 
 
-"""
-    Predict travel times of paths. The whole process can be splitted
-    into three steps.
-"""
-
-
-def predict():
-    # step1()
-
+def run():
+    """
+        Estimate travel times of each path. The whole process comprieses
+        three steps.
+    """
+    step1 = Step1()
     step2 = Step2()
+    step3 = Step3()
 
-    # step3()
+    step1.analyze()
+    step2.analyze()
+    step3.analyze()
 
 
 if __name__ == "__main__":
-    predict()
+    run()
